40a2f3ca80d293cfbf19bd5b8aa6680ac0200e1a
StarkShang <stark.shang@sjtu.edu.cn>
2018-04-22 12:29:12 +0800

    Startup this project
    Add class CommandBase

---
+++ Clap/CommandBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Clap
+{
+    public abstract class CommandBase
+    {
+		protected abstract Dictionary<string, CommandBase> CommandTable { get; }
+        protected abstract Dictionary<string, Action<Queue<string>>> OptionTable { get; }
+        protected abstract void DefaultBehavior(Queue<string> args);
+
+        public void Execute(Queue<string> args)
+        {
+            if (IsCommand(args)) CommandTable[args.Dequeue()].Execute(args);
+            if (IsOption(args)) OptionTable[args.Dequeue()](args);
+            DefaultBehavior(args);
+        }
+
+        public bool IsCommand(Queue<string> args)
+        {
+            return CommandTable != null
+                && args.TryPeek(out var token)
+                && CommandTable.ContainsKey(token);
+        }
+
+        public bool IsOption(Queue<string> args)
+        {
+            return OptionTable != null
+                && args.TryPeek(out var token)
+                && OptionTable.ContainsKey(token);
+        }
+    }
+}
